44b0ba1211d7dabb38d371cd3f577254a5e36a6a
gourav <gp1611gp@gmail.com>
2022-01-30 13:27:23 +0530

    yaha tak maine preroseesar ke bare me bana liya hai

---
+++ predefined_macros.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+int main(){
+
+
+ printf("File name is :- %s",__FILE__);
+ printf("\n");
+ printf("Date is is :- %s",__DATE__);
+ printf("\n");
+ printf("TIME is is :- %s",__TIME__);
+ printf("\n");
+ printf("LINE is is :- %d",__LINE__);
+ printf("\n");
+ printf("ANSI :- %d ",__STDC__);
+
+ printf("\n");
+ return 0;
+}
---
+++ prreprosesar.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#define pi  3.14
+#define sqtr(r) r*r
+
+int main(){
+
+    float var = pi;
+    int r = 4;
+    printf("pi is %f",var);
+    printf("\n");
+    printf("area of circal is %f",pi*sqtr(r));
+    printf("\n");
+
+
+
+
+
+
+ return 0;
+}
